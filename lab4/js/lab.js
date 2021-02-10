00560308cfbfc3640e5b89c00e2dc08da72d9e95
mbedolla863 <70286101+mbedolla863@users.noreply.github.com>
2021-02-09 21:05:49 -0800

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,9 @@
+// Pseudo-code for interactive fire extinguisher model
+  // Create large display block for simulation
+    // Enter “click to start” so it can start
+// First show fire and make fire extinguisher clickable
+  // Zoom on extinguisher, and interact step by step on what to do by clicking the directions
+    // Make the user wave mouse back and forth to put out virtual fire.
+      // End by putting “congratulations”
+// Test for errors
+  // publish
