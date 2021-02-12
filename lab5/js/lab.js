d92e2ac972c9b8383934b5d9e52f036ebb51fe75
mbedolla863 <70286101+mbedolla863@users.noreply.github.com>
2021-02-12 14:48:54 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+//
+//Author: Miguel Bedolla <mbedolla@csumb.edu>
+// Created: February 12
+
+// Define Varibales
+var make = "Hyundai";
+var model = "Elantra";
+var color = "White";
+var year = 2015;
+
+// calculate
+var age = 2021 - year
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " year<br>");
