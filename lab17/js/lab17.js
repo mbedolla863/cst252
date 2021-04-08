4749b4e7999b24a402914ec1d21faa48e5cb090f
mbedolla863 <70286101+mbedolla863@users.noreply.github.com>
2021-04-08 00:39:03 -0700

    lab 17

---
+++ lab17/js/lab17.js
@@ -0,0 +1,15 @@
+//
+//Author: Miguel Bedolla <mbedolla@csumb.edu>
+// Created: March 22 2021
+
+$("#summary-button").click(function(){
+	$("#first").toggleClass("kickflip");
+})
+
+$("#difficulties-button").click(function(){
+	$("#second").toggleClass("heelflip");
+})
+
+$("#results-button").click(function(){
+	$("#third").toggleClass("varial");
+})
